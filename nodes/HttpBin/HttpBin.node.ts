a8dfdf9df4515b7ed09249c589133e3df88c4872
OrionDesign <contato@oriondesign.art.br>
2024-08-20 03:19:17 -0300

    fix

--- nodes/HttpBin/HttpBin.node.ts
+++ nodes/HttpBin/HttpBin.node.ts
@@ -109,7 +109,7 @@ export class HttpBin implements INodeType {
 			if (proxySettings && Object.keys(proxySettings).length > 0) {
 				Object.assign(body, {
 					proxyHost: proxySettings.proxyHost || "",
-					proxyPort: proxySettings.proxyPort || "",
+					proxyPort: proxySettings.proxyPort || 1234,
 					proxyProtocol: proxySettings.proxyProtocol || "",
 					proxyUsername: proxySettings.proxyUsername || "",
 					proxyPassword: proxySettings.proxyPassword || "",
